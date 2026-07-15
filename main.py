4e15e34ca33663876ea23579396288908c2193e6
doublejjjjj <you@example.com>
2026-07-15 17:00:46 +0800

    fix: add Procfile and dynamic PORT

--- main.py
+++ main.py
@@ -1241,4 +1241,5 @@ async def analysis_page():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
